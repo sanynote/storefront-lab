84afdfbdabc5d4327b73f0d71b570c6081734812
sanynote <sunnyjuice94@gmail.com>
2025-05-30 16:58:35 +0900

    feat: 프로젝트 세팅

---
+++ src/pages/product-create.tsx
@@ -0,0 +1,7 @@
+export default function ProductCreatePage() {
+  return (
+    <div className="p-8 min-h-screen bg-red-100 text-black text-2xl">
+      📝 상품 생성 페이지
+    </div>
+  );
+}
---
+++ src/pages/products.tsx
@@ -0,0 +1,3 @@
+export default function ProductsPage() {
+  return <div className="p-8 bg-blue-600">상품 리스트 페이지</div>;
+}
